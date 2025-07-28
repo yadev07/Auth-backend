7f96aa2dc096bc70bc198611a31c3724362807b3
yadev07 <yadevsingh.nishad@gmail.com>
2025-07-28 14:36:56 +0530

    .

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 4000 ;
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['https://auth-yadev.netlify.app']
 
 app.use(express.json());
 app.use(cookieParser());
