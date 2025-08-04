867382c1bd4c80b88fcd354100b2f5d86e432c96
yadev07 <yadevsingh.nishad@gmail.com>
2025-08-05 00:31:24 +0530

    ..

--- middleware/userAuth.js
+++ middleware/userAuth.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const userAuth = async (req, res, next) => {
-    const { token } = req.cookies;
+    const { token } = req.cookies || req.headers.authorization || {};
 
     if (!token) {
         return res.status(401).json({ success: false, message: 'Not Authorized..!, Login again.' });
