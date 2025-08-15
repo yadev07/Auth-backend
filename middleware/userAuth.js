0e4520cf2bb68782103de3be620bcc0c11b21740
yadev07 <yadevsingh.nishad@gmail.com>
2025-08-15 13:53:21 +0530

    File Added

--- middleware/userAuth.js
+++ middleware/userAuth.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const userAuth = async (req, res, next) => {
-    const { token } = req.cookie;
+    const { token } = req.cookies || req.headers;
 
     if (!token) {
         return res.status(401).json({ success: false, message: 'Not Authorized..!, Login again.' });
